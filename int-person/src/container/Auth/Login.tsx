f09b14e910d593ae4a04bc8c2040e18dce95f789
TrueVermelho <edu.roqueamaral09@gmail.com>
2025-11-19 11:09:18 -0300

    Login

--- int-person/src/container/Auth/Login.tsx
+++ int-person/src/container/Auth/Login.tsx
@@ -1,9 +1,42 @@
-'use client';
+// src/container/Auth/Login.tsx
+"use client";
 
-import { ReactNode } from "react";
+import LoginForm from "@/components/auth/login/LoginForm";
+import { useAuth } from "@/hooks/useAuth";
+import { auth } from "@/lib/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function Login(): ReactNode {
+export default function Login() {
+  const router = useRouter();
+  const { loading } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [erroMensagem, setErroMensagem] = useState<string | null>(null);
+
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      router.push("/dashboard");
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error("ERRO LOGIN:", err);
+        setErroMensagem(err.message);
+      }
+    }
+  }
+
   return (
-    <></>
+    <LoginForm
+      handleLogin={handleLogin}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      loading={loading}
+      erroMensagem={erroMensagem}
+    />
   );
 }
