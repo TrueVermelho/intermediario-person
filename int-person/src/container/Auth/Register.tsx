6f6084e094ff8b8cd56084b1c885c61346f9b549
TrueVermelho <edu.roqueamaral09@gmail.com>
2025-11-19 19:14:37 -0300

    Register rota funcionando

--- int-person/src/container/Auth/Register.tsx
+++ int-person/src/container/Auth/Register.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import RegisterForm from "@/components/auth/register/RegisterForm";
+import { useAuth } from "@/hooks/useAuth";
 import { auth, db } from "@/lib/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Register() {
   const router = useRouter();
+  const { user } = useAuth();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  // Se o usuário já estiver logado → redireciona
+  useEffect(() => {
+    if (user) router.push("/dashboard");
+  }, [user, router]);
 
   async function handleRegister(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -27,7 +34,6 @@ export default function Register() {
         createdAt: serverTimestamp(),
       });
 
-      router.push("/dashboard");
     } catch (err: unknown) {
       console.error(err);
       alert(err);
